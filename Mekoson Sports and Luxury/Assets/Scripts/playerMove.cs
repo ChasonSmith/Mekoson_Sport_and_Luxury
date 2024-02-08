c6ba3cbd0d7a8034b498e3373695d4cb7330c673
ChasonSmith <cpscz6@umsystem.edu>
2024-02-08 16:07:29 -0600

    movement 1.01

--- Mekoson Sports and Luxury/Assets/Scripts/playerMove.cs
+++ Mekoson Sports and Luxury/Assets/Scripts/playerMove.cs
@@ -29,50 +29,94 @@ public class playerMove : MonoBehaviour
         // if (Input.GetKeyDown("space")){
         //     rb.velocity = new Vector3(rb.velocity.x,5,rb.velocity.z);
         // }
-        if (Input.GetKey(KeyCode.W)){
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D)){
             Vector3 forwardDirection = transform.forward;
             transform.Translate(forwardDirection * moveSpeed * Time.deltaTime, Space.World);
-
-            if (childTransform != null)
-            {
-                childTransform.rotation = Quaternion.LookRotation(forwardDirection);
-            }
-        }
-        if (Input.GetKey(KeyCode.D)){
             Vector3 rightDirection = transform.right;
             transform.Translate(rightDirection * moveSpeed * Time.deltaTime, Space.World);
 
-            if (childTransform != null)
-            {
-                childTransform.rotation = Quaternion.LookRotation(rightDirection);
-            }
+            Quaternion targetRotation = Quaternion.Lerp(Quaternion.LookRotation(forwardDirection), Quaternion.LookRotation(rightDirection), 0.5f); // This will make it look halfway between forward and left, adjust as needed
+            // Apply the interpolated rotation to the child transform
+            childTransform.rotation = targetRotation;
         }
-        if (Input.GetKey(KeyCode.S)){
-            Vector3 backwardDirection = -transform.forward; // Get the backward direction relative to the character's current orientation
-            transform.Translate(backwardDirection * moveSpeed * Time.deltaTime, Space.World);
-
-            if (childTransform != null)
-            {
-                childTransform.rotation = Quaternion.LookRotation(backwardDirection);
-            }
-        }
-        if (Input.GetKey(KeyCode.A)){
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A)){
+            Vector3 forwardDirection = transform.forward;
+            transform.Translate(forwardDirection * moveSpeed * Time.deltaTime, Space.World);
             Vector3 leftDirection = -transform.right;
             transform.Translate(leftDirection * moveSpeed * Time.deltaTime, Space.World);
 
-            if (childTransform != null)
-            {
-                childTransform.rotation = Quaternion.LookRotation(leftDirection);
+            Quaternion targetRotation = Quaternion.Lerp(Quaternion.LookRotation(forwardDirection), Quaternion.LookRotation(leftDirection), 0.5f); // This will make it look halfway between forward and left, adjust as needed
+            // Apply the interpolated rotation to the child transform
+            childTransform.rotation = targetRotation;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)){
+            Vector3 backwardDirection = -transform.forward;
+            transform.Translate(backwardDirection * moveSpeed * Time.deltaTime, Space.World);
+            Vector3 leftDirection = -transform.right;
+            transform.Translate(leftDirection * moveSpeed * Time.deltaTime, Space.World);
+
+            Quaternion targetRotation = Quaternion.Lerp(Quaternion.LookRotation(backwardDirection), Quaternion.LookRotation(leftDirection), 0.5f); // This will make it look halfway between forward and left, adjust as needed
+            // Apply the interpolated rotation to the child transform
+            childTransform.rotation = targetRotation;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D)){
+            Vector3 backwardDirection = -transform.forward;
+            transform.Translate(backwardDirection * moveSpeed * Time.deltaTime, Space.World);
+            Vector3 rightDirection = transform.right;
+            transform.Translate(rightDirection * moveSpeed * Time.deltaTime, Space.World);
+
+            Quaternion targetRotation = Quaternion.Lerp(Quaternion.LookRotation(backwardDirection), Quaternion.LookRotation(rightDirection), 0.5f); // This will make it look halfway between forward and left, adjust as needed
+            // Apply the interpolated rotation to the child transform
+            childTransform.rotation = targetRotation;
+        }
+        else{
+            if (Input.GetKey(KeyCode.W)){
+                Vector3 forwardDirection = transform.forward;
+                transform.Translate(forwardDirection * moveSpeed * Time.deltaTime, Space.World);
+
+                if (childTransform != null)
+                {
+                    childTransform.rotation = Quaternion.LookRotation(forwardDirection);
+                }
+            }
+            if (Input.GetKey(KeyCode.D)){
+                Vector3 rightDirection = transform.right;
+                transform.Translate(rightDirection * moveSpeed * Time.deltaTime, Space.World);
+
+                if (childTransform != null)
+                {
+                    childTransform.rotation = Quaternion.LookRotation(rightDirection);
+                }
+            }
+            if (Input.GetKey(KeyCode.S)){
+                Vector3 backwardDirection = -transform.forward; // Get the backward direction relative to the character's current orientation
+                transform.Translate(backwardDirection * moveSpeed * Time.deltaTime, Space.World);
+
+                if (childTransform != null)
+                {
+                    childTransform.rotation = Quaternion.LookRotation(backwardDirection);
+                }
+            }
+            if (Input.GetKey(KeyCode.A)){
+                Vector3 leftDirection = -transform.right;
+                transform.Translate(leftDirection * moveSpeed * Time.deltaTime, Space.World);
+
+                if (childTransform != null)
+                {
+                    childTransform.rotation = Quaternion.LookRotation(leftDirection);
+                }
             }
         }
         if (Input.GetKey("left")){
             //rb.velocity = new Vector3(-5,rb.velocity.y,rb.velocity.z);
             rotationAmount = rotationSpeed * Time.deltaTime;
+            childTransform.Rotate(Vector3.up, rotationAmount);
             transform.Rotate(Vector3.up, -rotationAmount);
         }
         if (Input.GetKey("right")){
             //rb.velocity = new Vector3(5,0,rb.velocity.z);
             rotationAmount = rotationSpeed * Time.deltaTime;
+            childTransform.Rotate(Vector3.up, -rotationAmount);
             transform.Rotate(Vector3.up, rotationAmount);
         }
     }   
