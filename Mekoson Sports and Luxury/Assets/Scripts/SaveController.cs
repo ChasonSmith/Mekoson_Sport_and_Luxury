d04fda900414d3dbacbea13158fbb8bf03630c37
Adam-McNeil <adam.mcneil@oasisdigital.com>
2024-04-11 13:41:21 -0500

    added save file

--- Mekoson Sports and Luxury/Assets/Scripts/SaveController.cs
+++ Mekoson Sports and Luxury/Assets/Scripts/SaveController.cs
@@ -7,6 +7,10 @@ using System;
 public class SaveController : MonoBehaviour
 {
     public Transform player;
+    public GameObject ownedCars;
+    public GameObject restOfCars;
+    public List<GameObject> cars;
+
     void Start() {
         
     }
@@ -23,18 +27,58 @@ public class SaveController : MonoBehaviour
 
     void Save() {
         string path = Application.persistentDataPath + "/test.txt";
-        string playerJson = JsonUtility.ToJson(new PlayerData(player));
-        File.WriteAllText(path, playerJson);
+
+        PlayerData playerData = new PlayerData(player);
+        InventoryData hotBarData = InventoryData.InventoryDataFactory(ownedCars);
+        InventoryData inventoryData = InventoryData.InventoryDataFactory(restOfCars);
+
+        string saveDataJson = JsonUtility.ToJson(new SaveData(playerData, hotBarData, inventoryData));
+
+        File.WriteAllText(path, saveDataJson);
     }
 
     void Load() {
         string path = Application.persistentDataPath + "/test.txt";
         StreamReader reader = new StreamReader(path);
-        string playerJson = reader.ReadToEnd();
-        PlayerData playerData = JsonUtility.FromJson<PlayerData>(playerJson);
-        player.position = playerData.position;
-        player.rotation = playerData.rotation;
+        string saveDataJson = reader.ReadToEnd();
+        SaveData saveData = JsonUtility.FromJson<SaveData>(saveDataJson);
+
+        player.position = saveData.playerData.position;
+        player.rotation = saveData.playerData.rotation;
+
+        LoadInventoryData(saveData.hotBarData, ownedCars);
+        LoadInventoryData(saveData.inventoryData, restOfCars);
+        
         reader.Close();
+    }
+
+    void LoadInventoryData(InventoryData inventoryData, GameObject parent) {
+        for (int i = 0; i < inventoryData.cars.Count; i++) {
+            GameObject child = parent.transform.GetChild(i).gameObject;
+            if (child.transform.childCount > 0) {
+                Destroy(child.transform.GetChild(0).gameObject);
+            }
+            foreach (GameObject car in cars) {
+                if (inventoryData.cars[i].name == car.name) {
+                    GameObject carGameObject= Instantiate(car);
+                    carGameObject.transform.parent = child.transform;
+                    carGameObject.SetActive(false);
+                }
+            }
+        }
+    } 
+}
+
+[Serializable]
+public class SaveData {
+    public PlayerData playerData;
+    public InventoryData hotBarData;
+    public InventoryData inventoryData;
+
+    public SaveData(PlayerData playerData, InventoryData hotBarData, InventoryData inventoryData) {
+        this.playerData = playerData;
+        this.hotBarData = hotBarData;
+        this.inventoryData = inventoryData;
     }
 }
 
@@ -50,5 +94,45 @@ public class PlayerData {
     public PlayerData(Transform transform) {
         this.position = transform.position;
         this.rotation = transform.rotation;
+    }
+}
+
+[Serializable]
+public class InventoryData{
+    public List<CarData> cars;
+
+    public InventoryData() {
+        cars = new List<CarData>();
+    }
+
+    static public InventoryData InventoryDataFactory(GameObject parent) {
+        InventoryData inventoryData = new InventoryData();
+        for (int i = 0; i < parent.transform.childCount; i++) {
+            GameObject child = parent.transform.GetChild(i).gameObject;
+            if (child.transform.childCount > 0) {
+                string name = child.transform.GetChild(0).gameObject.name.Replace("(Clone)", "");
+                inventoryData.Add(new CarData(name));
+            } else {
+                inventoryData.Add(new CarData("EMPTY"));
+            }
+        }
+        return inventoryData;
+    }
+
+
+    public void Add(CarData carData) {
+        cars.Add(carData);
+    }
+
+
+
+}
+
+[Serializable]
+public class CarData {
+    public string name;
+
+    public CarData(string name) {
+        this.name = name;
     }
 }
